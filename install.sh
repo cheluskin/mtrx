fbd9a3a3fd0c83723b34c8c75fc9890c58d4260e
cheluskin <bystrov1984@gmail.com>
2022-06-04 10:54:32 +0300

    last

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ MY_IP=$(curl http://checkip.amazonaws.com)
 DOMAIN_IP=$(dig +short $DOMAIN | head -1)
 
 if [[ "$MY_IP" != "$DOMAIN_IP" ]] ; then
-    echo "Domain not resolve A record to ip $my_ip"
+    echo "Domain not resolve A record to ip $MY_IP"
     exit 1
 fi
 
@@ -23,7 +23,7 @@ sudo wget -O /usr/share/keyrings/matrix-org-archive-keyring.gpg https://packages
 echo "deb [signed-by=/usr/share/keyrings/matrix-org-archive-keyring.gpg] https://packages.matrix.org/debian/ $(lsb_release -cs) main" |
     sudo tee /etc/apt/sources.list.d/matrix-org.list
 sudo apt update -y
-sudo apt install -y matrix-synapse-py3 libpq5 nginx cron socat
+sudo apt install -y nginx cron socat
 
 curl https://get.acme.sh | sh
 export LE_WORKING_DIR="/root/.acme.sh"
@@ -38,5 +38,4 @@ acme.sh --issue -d $DOMAIN --webroot /var/www/html
 mkdir -p /etc/mtrxcerts/
 acme.sh --install-cert -d $DOMAIN --key-file /etc/mtrxcerts/key.pem --fullchain-file /etc/mtrxcerts/fullchain.pem --reloadcmd "systemctl reload nginx.service"
 
-
-
+sudo apt install -y matrix-synapse-py3 libpq5
